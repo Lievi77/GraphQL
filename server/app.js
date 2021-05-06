98ed88cfeb54a354de69a7cd8e5a16f4344adf30
Lev <lguzm038@uottawa.ca>
2021-05-06 14:07:25 -0400

    Created app.js and configured .gitignore

---
+++ server/app.js
@@ -0,0 +1,9 @@
+const express = require('express'); //ES6 syntax
+
+const app = express();
+
+app.listen(4000, () => { //usually located it localhost:4000
+
+    console.log('Listening for requests ');
+
+});
