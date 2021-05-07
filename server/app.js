f9a3e0b8c9f65369e7e18565559fcb53f755faff
Lev <lguzm038@uottawa.ca>
2021-05-07 12:12:44 -0400

    Formatted app.js

--- server/app.js
+++ server/app.js
@@ -1,19 +1,22 @@
-const express = require('express'); 
-const {graphqlHTTP} = require('express-graphql');
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
 
 //make our schema be known/inferred to our app
-const schema = require('./schema/schema'); //no need to add .js 
+const schema = require("./schema/schema"); //no need to add .js
 
 const app = express();
 
 //.use lets us pass an endpoint.
-app.use('/graphql', graphqlHTTP({
-    //ie, we see the graphiql playground 
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    //ie, we see the graphiql playground
     graphiql: true,
-    schema //ES6 syntax
-}) );
+    schema, //ES6 syntax
+  })
+);
 
-
-app.listen(4000, () => { //usually located it localhost:PORTNUMBER
-    console.log('Listening for requests ');
+app.listen(4000, () => {
+  //usually located it localhost:PORTNUMBER
+  console.log("Listening for requests ");
 });
