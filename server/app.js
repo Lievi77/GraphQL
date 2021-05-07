b8e0b3f10e3d4b0e4a38873170c5520ca3a4198f
Lev <lguzm038@uottawa.ca>
2021-05-07 10:53:40 -0400

    Added schema

--- server/app.js
+++ server/app.js
@@ -1,12 +1,16 @@
 const express = require('express'); 
 const {graphqlHTTP} = require('express-graphql');
 
+//make our schema be known/inferred to our app
+const schema = require('./schema/schema'); //no need to add .js 
+
 const app = express();
 
 //.use lets us pass an endpoint.
 app.use('/graphql', graphqlHTTP({
     //ie, we see the graphiql playground 
-    graphiql: true
+    graphiql: true,
+    schema //ES6 syntax
 }) );
 
 
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -6,17 +6,52 @@ const {
     GraphQLObjectType,
     GraphQLID, 
     GraphQLString,
-    GraphQLInt
+    GraphQLInt,
+    GraphQLSchema
 } = graphql
 
-
 //Create new types/objs
-const UserTypes = new GraphQLObjectType({
+const UserType = new GraphQLObjectType({
     name : 'User', 
     description: 'Documentation goes here',
     fields: () => ({
-        id: {type: GraphQLID},
+        id: {type: GraphQLString},
         name: {type: GraphQLString} ,
         age: {type: GraphQLInt},
     })
 })
+
+//! IMPORTANT: ROOT QUERY
+// * We have to tell GraphQL how our queries will be structured
+// * ie, what to return
+const RootQuery = new GraphQLObjectType(
+    {
+        name: 'RootQueryType',
+        description: 'Root Query Definition',
+        fields: {
+
+            user: {
+                type: UserType,
+                //argument to use when we ask for a specific obj instance
+                args: {id: {type: GraphQLString}},
+
+                resolve(parent,args){
+                    //we resolve with data
+                    // ie, we fetch and return using the information provided
+                    // think of it as an entry point to our graph/network
+
+                    //get and return data from datasource / database
+
+                    
+
+                }
+            }
+
+        }
+    }
+);
+
+//let the Schema be known to our app
+module.exports = new GraphQLSchema(
+    {query: RootQuery}
+);
