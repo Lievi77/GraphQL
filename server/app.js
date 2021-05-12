fa8477507c08b3638f8e106cb4e5d4eb15debd64
Lev <lguzm038@uottawa.ca>
2021-05-12 11:13:52 -0400

    Modified .gitignore and installed dotenv

--- server/app.js
+++ server/app.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
+const mongoose = require("mongoose"); //for mongoDB connection
+require("dotenv").config(); //loads .env credentials on process.env
 
+mongoose.connect(
+  `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@gql-cluster-sandbox.yfdby.mongodb.net/${process.env.MONGODB_DBNAME}?retryWrites=true&w=majority`,
+  { useNewUrlParser: true, useUnifiedTopology: true } // removes deprecated warnings
+);
+mongoose.connection.once("open", () => {
+  console.log("Connection successful");
+});
 //make our schema be known/inferred to our app
 const schema = require("./schema/schema"); //no need to add .js
 const testSchema = require("./schema/types_schema"); //no need to add .js
